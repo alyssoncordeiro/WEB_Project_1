427fe4d68011e4d40c1456bc99f7e3ffb418be14
Alysson de Almeida Cordeiro <33531973+alyssoncordeiro@users.noreply.github.com>
2020-09-15 20:51:26 -0300

    Updates

---
+++ precache-manifest.5eb36885466c4ae08d9a1ef170fc6352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c35e4ee44156217b1834228d52bbe0",
+    "url": "/WEB_Project_1/index.html"
+  },
+  {
+    "revision": "e1613206c978e5f35b52",
+    "url": "/WEB_Project_1/static/css/main.88380823.chunk.css"
+  },
+  {
+    "revision": "3fbec5e741bc6d958d39",
+    "url": "/WEB_Project_1/static/js/2.de030556.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WEB_Project_1/static/js/2.de030556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1613206c978e5f35b52",
+    "url": "/WEB_Project_1/static/js/main.513019ad.chunk.js"
+  },
+  {
+    "revision": "d945431283376a37a089",
+    "url": "/WEB_Project_1/static/js/runtime-main.f44a53ce.js"
+  }
+]);
